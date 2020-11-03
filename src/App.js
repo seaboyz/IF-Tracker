97a236dc96381d345cfa617a3d8450cf075deb72
seaboyz <seaboyz@hotmail.com>
2020-11-03 00:32:26 -0600

    Initialize state  add hanlerChange handlerSubmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react'
 
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import FastsHistory from './components/FastsHistory'
 import FastForm from './components/FastForm'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fasts: [],
+      newFast: {},
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    // using one hanler to update the newFast
+    const newFast = {
+      ...this.state.newFast,
+      [event.target.name]: event.target.value,
+    }
+    this.setState({ newFast })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const fasts = [
+      ...this.state.fasts,
+      {
+        id: Date.now(),
+        ...this.state.newFast,
+      },
+    ]
+    const newFast = {}
+    this.setState({ fasts, newFast })
+  }
+
   render() {
     return (
       <div>
-        <FastForm />
-        <FastsHistory />
+        <FastForm
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          start={this.state.newFast.start}
+          finish={this.state.newFast.finish}
+          weight={this.state.newFast.weight}
+          feeling={this.state.newFast.feeling}
+          comment={this.state.newFast.comment}
+        />
+        <FastsHistory fasts={this.state.fasts}/>
       </div>
     )
   }
