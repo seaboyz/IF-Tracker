a3eb36a27d43a644b7efd5b38564b1352f184e37
seaboyz <seaboyz@hotmail.com>
2020-11-03 00:34:32 -0600

    Add FastsHistory component

--- src/components/FastsHistory.js
+++ src/components/FastsHistory.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 import FastCard from './FastCard'
 
-import fastCardBackgroudColors from './fastCardBackgroudColors'
-
-function FastsHistory() {
-  return fastCardBackgroudColors
-    .slice(0, 7) // number of Fast cards show on the landing page
-    .map((variant, idx) => <FastCard variant={variant} idx={idx} />)
+function FastsHistory({ fasts }) {
+  return fasts.map(fast => (
+    <FastCard
+      key={fast.id}
+      id={fast.id}
+      start={fast.start}
+      finish={fast.finish}
+      weight={fast.weight}
+      feeling={fast.feeling}
+      comment={fast.comment}
+    />
+  ))
 }
 
 export default FastsHistory
