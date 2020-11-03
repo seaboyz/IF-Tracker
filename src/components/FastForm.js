1d064a458ebed0405307ee032280f60fbb5ccb83
seaboyz <seaboyz@hotmail.com>
2020-11-03 00:33:56 -0600

    Finish FastForm.js

--- src/components/FastForm.js
+++ src/components/FastForm.js
@@ -1,9 +1,80 @@
 import React from 'react'
+import { Button, Form } from 'react-bootstrap'
 
+function FastForm({
+  handleSubmit,
+  handleChange,
+  start,
+  finish,
+  weight,
+  feeling,
+  comment,
+}) {
+  return (
+    <Form onSubmit={handleSubmit} autoComplete='off'>
+      <Form.Group>
+        <Form.Label for='start'>start</Form.Label>
+        <Form.Control
+          type='text'
+          name='start'
+          id='start'
+          value={start}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label for='finish'>finish:</Form.Label>
+        <Form.Control
+          type='text'
+          name='finish'
+          id='finish'
+          value={finish}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label for='weight'>weight:</Form.Label>
+        <Form.Control
+          type='weight'
+          name='weight'
+          id='weight'
+          value={weight}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label for='feeling'>feeling:</Form.Label>
+        <Form.Control
+          type='feeling'
+          name='feeling'
+          id='feeling'
+          value={feeling}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label for='comment'>comment:</Form.Label>
+        <Form.Control
+          type='comment'
+          name='comment'
+          id='comment'
+          value={comment}
+          onChange={handleChange}
+        />
+      </Form.Group>
 
-
-function FastForm() {
-  return <h1>Form</h1>
+      <Button variant='primary' type='submit'>
+        Submit
+      </Button>
+      <Button
+        className='btn-success'
+        type='reset'
+        style={{ background: 'darkred' }}
+      >
+        Reset
+      </Button>
+    </Form>
+  )
 }
 
 export default FastForm
