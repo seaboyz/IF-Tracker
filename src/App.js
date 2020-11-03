7a4677d2eb0d1e6a07427b94b268d2e2fdd79fbe
seaboyz <seaboyz@hotmail.com>
2020-11-02 21:40:50 -0600

    Change App.js from function to class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import FastsHistory from './components/FastsHistory'
 import FastForm from './components/FastForm'
 
-function App() {
-  return (
-    <div>
-      <FastForm />
-      <FastsHistory />
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <FastForm />
+        <FastsHistory />
+      </div>
+    )
+  }
 }
 
 export default App
