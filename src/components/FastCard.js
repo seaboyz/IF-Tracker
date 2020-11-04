76e880174b8c26de8ead9febae9695fab61702c5
seaboyz <seaboyz@hotmail.com>
2020-11-04 15:37:42 -0600

    Change weight feeling and comment to conditional rendering

--- src/components/FastCard.js
+++ src/components/FastCard.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 import { Card } from 'react-bootstrap'
 
-function FastCard({ id, date,start, finish, weight, feeling, comment }) {
-
+function FastCard({ id, date, start, finish, weight, feeling, comment }) {
   return (
     <Card
       bg='primary'
@@ -13,17 +12,22 @@ function FastCard({ id, date,start, finish, weight, feeling, comment }) {
       className='mb-2 mx-auto'
     >
       <Card.Header>
-      {/* need to be converted to mm-dd-yyyy */}
-        {date.slice(0,10)}
+        {/* need to be converted to mm-dd-yyyy */}
+        {date.slice(0, 10)}
       </Card.Header>
+
       <Card.Body>
         <Card.Title>Intermittent</Card.Title>
+
         <Card.Title>
           From {start} To {finish}
         </Card.Title>
-        <Card.Title>{weight} LB</Card.Title>
-        <Card.Title>Feeling {feeling}</Card.Title>
-        <Card.Title>Comments {comment}</Card.Title>
+
+        {/* conditional render weight, Feeling and Comments */}
+        {/* if user has input , card will show it, vice versa*/}
+        {weight && <Card.Title>{weight} LB</Card.Title>}
+        {feeling && <Card.Title>Feeling: {feeling}</Card.Title>}
+        {comment && <Card.Title>Comments: {comment}</Card.Title>}
       </Card.Body>
     </Card>
   )
