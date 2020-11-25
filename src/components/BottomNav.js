1745009fbd08553f762d26efc4d02bdaeac811b7
JustinPrejean <JustinPrejean1@gmail.com>
2020-11-24 19:22:03 -0600

    combined css

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Breadcrumb } from "react-bootstrap";
-import "../BottomNav.css";
+import "../index.css";
 
 function BottomNav() {
   return (
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../ContactForm.css";
+import "../index.css";
 
 import { Form, Button } from "react-bootstrap";
 
