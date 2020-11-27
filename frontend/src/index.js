8d9667b13e341af4ab62b14882c12c8de1716ac6
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-26 21:47:16 -0600

    Add react-router-dom to app.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import App from './App'
-import IfTrackerScreen from './screens/IfTrackerScreen'
+import App from './App'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
     <React.StrictMode>
-        <IfTrackerScreen />
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 )
